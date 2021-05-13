0465ac37a637858dabe4a1b7b2c8764abf730c4d
pak <rupaklamadev@gmail.com>
2021-05-13 17:11:17 -0400

    advanced jest matchers

---
+++ basic/src/matchers.spec.js
@@ -0,0 +1,67 @@
+// Jest Matchers
+
+test('', async () => {
+  const a = {
+    foo: {
+      num: 1,
+    },
+  };
+
+  // promises/async
+  const res = new Promise(resolve => {
+    resolve(a);
+  });
+
+  const rej = new Promise((resolve, reject) => {
+    reject(a);
+  });
+
+  await expect(res).resolves.toEqual({
+    // resolve object to be
+    foo: {
+      num: 1,
+    },
+  });
+
+  // when using 'resolves/rejects' for Promise always 'return' the final statement
+  // or use async await for each expectation
+  // return expect(rej).rejects.toEqual({
+  await expect(rej).rejects.toEqual({
+    // resolve object to be
+    foo: {
+      num: 1,
+    },
+  });
+});
+
+// Mock Functions
+
+// test function
+const fetchData = cb => {
+  const val = 'Data';
+
+  cb(val);
+};
+
+test('', async () => {
+  // jest mock function does not do anything
+  // place holder for any related function when testing
+  // in this case, some sort of 'callback' function - just to be fulfill the requirements
+  const cb = jest.fn();
+  fetchData(cb);
+
+  // NOTE - .toHaveBeenCalled to ensure that a mock function got called.
+  //  expect(cb).toHaveBeenCalled();
+  expect(cb).toHaveBeenCalledWith('Data');
+});
+
+// Waiting with the Done Callback for Async actions
+test('', done => {
+  // needs add done param
+  // this is going to pass since 'Jest is synchronous test runner'
+  // this should fail the test unless call with 'done' Callback
+  setTimeout(() => {
+    expect(1).toBe(1);
+    done(); // calling done to tell jest everything is finished
+  }, 3000);
+});
